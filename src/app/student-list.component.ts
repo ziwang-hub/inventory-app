56d0d81348d35f2922b6eca487ebeb683a1a94a0
ziwang-hub <2324825234@qq.com>
2020-04-04 14:25:41 +0800

    提交

---
+++ src/app/student-list.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'student-list',
+    template: `
+    <student [name]="studentNames[0]" (studentSelected)="onStudentSelected($event)"></student>
+    `
+})
+
+export class StudentList {
+    studentNames = ['张三', '李四', '王五'];
+
+    constructor() {
+
+    }
+
+    onStudentSelected(name: string) {
+        console.log('选中了学生:' + name);
+    }
+}
---
+++ src/app/student.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter } from '@angular/core';
+
+
+@Component({
+    selector: 'student',
+    inputs: ['name'],
+    outputs: ['studentSelected'],
+    template: `
+      <p (click)="onClicked()">{{name}}</p>
+    `
+})
+
+export class Student {
+    name: string;
+    studentSelected: EventEmitter<string>;
+
+    constructor() {
+        this.studentSelected = new EventEmitter();
+    }
+
+    onClicked() {
+        this.studentSelected.emit(this.name);
+
+    }
+
+}
